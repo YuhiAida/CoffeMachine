1c53dd74790c7cc91a2fcb962d2e23364c49763c
Yuhi <Yuhi>
2018-08-09 23:19:19 -0300

    Funcionando todo menos el infusor

--- Saeco_Intelia_HD8751/Saeco_Intelia_HD8751.ino
+++ Saeco_Intelia_HD8751/Saeco_Intelia_HD8751.ino
@@ -1,53 +1,50 @@
-bool sensorPosicionBrew = 2;
-bool sensorBrewInPlace = 4;
-bool sensorTapaCerrada = 7;
-//bool sensorAgua = ;
-bool encender = 8;
+/***   Included libraries  ***/
 
-/*falta sensor de agua,
-  sensor del molino atorado o libre(falta de granos),
-  boton de molino o ya molido,
-  boton de limpieza,
-  boton agua hirviendo,
-  botones de cafes
+/***   Global variables and function definition  ***/
+const int cafe_chico = 7;
 
-  posibilidad de sacar 2 cafes apredando 2 veces, aprox 1,32s.
-*/
-
-//el molino se acciona aproximadamente 10 segundos, para cafe largo 20 segundos.
-//un Espresso tarda aproximadamente 40 segundos en total 5 mas para limpiar el infusor.
-//cafe largo tarda 1,35s.
-//vapor 28 segundos para calentar
-
-bool sensor= false;
-bool brewInPlace = false;
-bool running = false;
-
+/***   Setup  ***/
 void setup() {
-  pinMode(sensorPosicionBrew, INPUT);
-  pinMode(sensorBrewInPlace, INPUT);
-  pinMode(sensorTapaCerrada, INPUT);
-  pinMode(encender, INPUT);
+    pinMode(cafe_chico, INPUT);
+    pinMode(12, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(9, OUTPUT);
+
 }
 
+
+/***   Loop  ***/
 void loop() {
-  encender()
-
-  /*
-  if(digitalRead(encender) == true){
-    digitalRead(sensorBrewInPlace) == true;
-    while(sensorBrewInPlace == true && sensorTapaCerrada && true){
-      /*falta el del agua y residuos, para el display necesitaria un switch
-        para mostrar imagen segun el caso
-        //Calentar el agua 15s aprox.
-        //Encender Bomba despues de los 15 segundos iniciales encender bomba por 25s, un total de 40.
-        //Verificar que este limpio el "brew".
-        //Esperar orden.
-
-      }
-    }*/
-}
-
-void encender(){
-
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    if (digitalRead(cafe_chico) == 1) {
+        /*
+        calentar
+        */
+        digitalWrite(12, LOW);
+        delay(8000);
+        digitalWrite(12, HIGH);
+        /*
+        moler
+        */
+        digitalWrite(11, LOW);
+        delay(5000);
+        digitalWrite(11, HIGH);
+        delay(500);
+        /*
+        mover infusor hasta fin de carrera
+        */
+        digitalWrite(10, LOW);
+        digitalWrite(12, LOW);
+        delay(5000);
+        digitalWrite(10, HIGH);
+        /*
+        bomba
+        */
+        digitalWrite(9, LOW);
+        delay(18000);
+    }
 }
